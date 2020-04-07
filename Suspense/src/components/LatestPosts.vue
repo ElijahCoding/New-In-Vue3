d1b03661600e726d866db8c83cbc70019a60018d
ElijahCoding <cuiyeqing960904@gmail.com>
2020-04-07 14:43:12 +0300

    refactored structure

---
+++ Suspense/src/components/LatestPosts.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        {{ posts }}
+    </div>
+</template>
+
+<script>
+    import axios from 'axios'
+
+    export default {
+        async setup () {
+            const posts = await axios.get('https://jsonplaceholder.typicode.com/todos')
+
+            return {
+                posts
+            }
+        }
+    }
+</script>
