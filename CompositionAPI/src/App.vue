21a8962aa6bf0101479cf87129b103dfcb6d721b
ElijahCoding <cuiyeqing960904@gmail.com>
2020-04-07 17:43:04 +0300

    passing dynamic props

--- CompositionAPI/src/App.vue
+++ CompositionAPI/src/App.vue
@@ -1,14 +1,25 @@
 <template>
   <div>
-    <Body text="display me!" />
+    <Body :text="text" />
+
+    <input type="text" v-model="text">
   </div>
 </template>
 
 <script>
+  import { ref } from 'vue'
   import Body from './components/Body.vue'
   export default {
     components: {
       Body
     },
+
+    setup () {
+      const text = ref('')
+
+      return {
+        text
+      }
+    }
   }
 </script>
